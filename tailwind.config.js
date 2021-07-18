db984b171ae805781e3c99dfd7c89a6830b60dbe
Lisa <llvvss@icloud.com>
2021-07-18 11:46:21 -0600

    Update tailwind.config.js
    added Tailwind plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -967,5 +967,10 @@ module.exports = {
     wordBreak: ['responsive'],
     zIndex: ['responsive', 'focus-within', 'focus'],
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
