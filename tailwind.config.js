708a7235c0e11106c18f965e730992e4e92219b9
Lisa <llvvss@icloud.com>
2021-07-18 11:59:59 -0600

    Update tailwind.config.js
    purge lines added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: [],
+  mode: 'jit',
+  purge: {
+  enabled: ["production", "staging"].includes(process.env.NODE_ENV),
+  content: [
+    './**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+  ],
+  },
   presets: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
