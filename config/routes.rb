c4f40f0d339914a4831957f31924c71e2b67e739
Lisa <llvvss@icloud.com>
2021-07-21 18:00:25 -0600

    deleted test page and reference to it

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,5 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'home/index'
   get "events", to: "home#events"
-  get "test", to: "home#test"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
